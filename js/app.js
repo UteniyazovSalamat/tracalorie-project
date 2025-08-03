0af2fb814ee8fc69fb12b987ca713f5820e42863
UteniyazovSalamat <sss19855891@gmail.com>
2025-08-03 13:37:33 -0600

    feat: add filterItems and reset

--- js/app.js
+++ js/app.js
@@ -48,6 +48,13 @@ class CalorieTracker {
         }
     }
 
+    reset() {
+        this._totalCalories = 0;
+        this._meals = [];
+        this._workouts = [];
+        this._render();
+    }
+
     // Private Methods
     _displayCaloriesTotal() {
         const totalCaloriesEl = document.getElementById('calories-total');
@@ -172,6 +179,12 @@ class App {
         document.getElementById('meal-items').addEventListener('click', this._removeItem.bind(this, 'meal'));
 
         document.getElementById('workout-items').addEventListener('click', this._removeItem.bind(this, 'workout'));
+
+        document.getElementById('filter-meals').addEventListener('keyup', this._filterItems.bind(this, 'meal'));
+
+        document.getElementById('filter-workouts').addEventListener('keyup', this._filterItems.bind(this, 'workout'));
+
+        document.getElementById('reset').addEventListener('click', this._reset.bind(this));
     }
 
     _newItem(type, e) {
@@ -213,6 +226,26 @@ class App {
                 e.target.closest('.card').remove();
             }
         }
+    }
+
+    _filterItems(type, e) {
+        const text = e.target.value.toLowerCase();
+        document.querySelectorAll(`#${type}-items .card`).forEach((item) => {
+            const name = item.firstElementChild.firstElementChild.textContent;
+            if (name.toLowerCase().indexOf(text) !== -1) {
+                item.style.display = 'block';
+            } else {
+                item.style.display = 'none';
+            }
+        });
+    }
+
+    _reset() {
+        this._tracker.reset();
+        document.getElementById('meal-items').innerHTML = '';
+        document.getElementById('workout-items').innerHTML = '';
+        document.getElementById('filter-meals').value = '';
+        document.getElementById('filter-workouts').value = '';
     }
 }
 const app = new App();
